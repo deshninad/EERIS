022fbaddb097861a6527b595f31523b221d91238
deshninad <deshninad77@gmail.com>
2025-03-25 23:52:35 -0400

    npm updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import { AuthProvider } from "./AuthProvider";
 import ProtectedRoute from "./ProtectedRoute"; 
 import AdminDashboard from "./pages/adminDashboard/adminDashboard";
 
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//          <Route path="/upload" element={<ProtectedRoute><EUploadPage /></ProtectedRoute>} />
-//          <Route path="/dashboard" element={<ProtectedRoute><ESummaryPage /></ProtectedRoute>} />
-//          <Route path="/admindashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-//         </Routes>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
+//  function App() {
+//    return (
+//      <AuthProvider>
+//        <Router>
+//          <Routes>
+//          <Route path="/" element={<LoginPage />} />
+//          <Route path="/login" element={<LoginPage />} />
+//           <Route path="/upload" element={<ProtectedRoute><EUploadPage /></ProtectedRoute>} />
+//           <Route path="/dashboard" element={<ProtectedRoute><ESummaryPage /></ProtectedRoute>} />
+//           <Route path="/admindashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+//          </Routes>
+//        </Router>
+//      </AuthProvider>
+//    );
+//  }
 
 
 //removing access controls for development
