ee62649780e9a2552af61c6078fde43511e2678e
Srishti Nagraj <119357026+srishtinagraj@users.noreply.github.com>
2025-04-28 12:00:17 -0400

    UI changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import dotenv from 'dotenv';
 import Tesseract from 'tesseract.js';
 import OpenAI from 'openai';
 
-dotenv.config();
+dotenv.config({ path: './src/backend/.env' })
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
